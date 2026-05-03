80bcbfb2ddba477dc1613c21d96abd064e27f294
alexbaza <alexdevmega@proton.me>
2026-05-03 09:31:58 -0600

    Fixed cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,9 @@ int main() {
         glfwPollEvents();
     }
 
+    vkDestroySurfaceKHR(vkContext.instance, WindowContext.surface, nullptr);
+    vkDestroyDevice(DeviceTypes.device, nullptr);
+
     if (enableValidationLayers) {
         DestroyDebugUtilsMessengerEXT(vkContext.instance, vkContext.debugMessenger, nullptr);
     }
